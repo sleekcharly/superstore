f4635ec7e7b10c9e266b9646ffa8521c1e4ff778
sleekcharly <c_ukasoanya@yahoo.com>
2024-04-17 16:11:46 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center flex-col min-h-screen">
+      <h2 className="text-4xl">Welcome to Superstore</h2>
+    </div>
+  );
+}
